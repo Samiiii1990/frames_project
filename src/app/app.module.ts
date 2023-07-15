ef1b2fdc63dfda3f546009fd16837d72ab214681
Samiiii1990 <sami.hermana@gmail.com>
2023-07-15 14:38:56 -0300

    firebase connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 //Módulos
 import { AppRoutingModule } from './app-routing.module';
 import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/compat/firestore/'; 
 
 //Componentes
 import { AppComponent } from './app.component';
 import { ListFramesComponent } from './components/list-frames/list-frames.component';
 import { CreateFrameComponent } from './components/create-frame/create-frame.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { provideStorage,getStorage } from '@angular/fire/storage';
+
 
 @NgModule({
   declarations: [
@@ -27,8 +27,10 @@ import { provideStorage,getStorage } from '@angular/fire/storage';
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
   ],
-  providers: [],
+  providers: [AngularFirestore],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/list-frames/list-frames.component.ts
+++ src/app/components/list-frames/list-frames.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Firestore, collectionData } from '@angular/fire/firestore';
+import { collection } from 'firebase/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-list-frames',
@@ -6,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-frames.component.css']
 })
 export class ListFramesComponent {
+  firestore: Firestore = inject(Firestore)
+  items$: Observable<any[]>;
 
+  constructor() {
+    const aCollection = collection(this.firestore, 'items')
+    this.items$ = collectionData(aCollection);
+  }
 }
